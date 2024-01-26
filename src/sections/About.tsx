2e667ddda2467b4ace1fac11960930af4ccd9eb7
cbxcvl <carloscavallinburatto@gmail.com>
2024-01-26 17:20:51 -0300

    feat: add About Section

---
+++ src/sections/About.tsx
@@ -0,0 +1,14 @@
+export const About = () => {
+  return (
+    <section id="sobre" className="h-96 w-full">
+      <div className="h-full w-full">
+        <h2>Seção 2</h2>
+        <p>
+          Bem-vindo à Seção agr. Este é um exemplo simples de um componente de
+          seção no Next.js. Você pode personalizar este componente conforme
+          necessário para atender aos requisitos da sua aplicação.
+        </p>
+      </div>
+    </section>
+  );
+};
