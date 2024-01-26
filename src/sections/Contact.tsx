1fd2716de8c2c9974a8b73680d185b68e0428833
cbxcvl <carloscavallinburatto@gmail.com>
2024-01-26 17:21:00 -0300

    feat: add Contect Section

---
+++ src/sections/Contact.tsx
@@ -0,0 +1,12 @@
+export const Contact = () => {
+  return (
+    <section id="contato">
+      <h2>Seção 4</h2>
+      <p>
+        Bem-vindo à Seção 4. Este é um exemplo simples de um componente de seção
+        no Next.js. Você pode personalizar este componente conforme necessário
+        para atender aos requisitos da sua aplicação.
+      </p>
+    </section>
+  );
+};
