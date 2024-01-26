917b5a0829924e6b5bf441cf85ff23fad28b8a83
cbxcvl <carloscavallinburatto@gmail.com>
2024-01-26 17:21:22 -0300

    feat: add Services Section

---
+++ src/sections/Services.tsx
@@ -0,0 +1,12 @@
+export const Services = () => {
+  return (
+    <section id="servicos">
+      <h2>Seção 3</h2>
+      <p>
+        Bem-vindo à Seção 3. Este é um exemplo simples de um componente de seção
+        no Next.js. Você pode personalizar este componente conforme necessário
+        para atender aos requisitos da sua aplicação.
+      </p>
+    </section>
+  );
+};
