623d60f647a9390f047865b006db5254ec58f907
cbxcvl <carloscavallinburatto@gmail.com>
2024-02-07 16:52:07 -0300

    feat: add  hero content component

--- src/components/HeroContent/HeroContent.tsx
+++ src/components/HeroContent/HeroContent.tsx
@@ -13,9 +13,9 @@ export function HeroContent({ content }: HeroContentProps): JSX.Element {
           x: 0,
           transition: { stiffness: 200, damping: 30, mass: 1 },
         }}
-        className="w-full lg:w-[23%] h-fit flex bg-transparent mt-20 "
+        className="w-full lg:w-[40%] h-fit flex bg-transparent mt-20 "
       >
-        <p className="w-full font-sans leading-none font-medium text-[1.75rem] text-wrap text-left ">
+        <p className="w-full font-sans leading-none font-semibold text-[1.75rem]  text-wrap text-left ">
           {content}
         </p>
       </m.div>
