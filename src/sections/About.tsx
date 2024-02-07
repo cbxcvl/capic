9daedbceed2c6b6a3793a37ad37b9fac92bdf7ac
cbxcvl <carloscavallinburatto@gmail.com>
2024-02-07 16:53:29 -0300

    feat: add about to page

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,14 +1,26 @@
+'use client';
+
+import { LazyMotion, domAnimation, m } from 'framer-motion';
+
+import { BadgeContent } from '../components/BadgeContent/BadgeContent';
+
 export const About = () => {
   return (
-    <section id="sobre" className="h-96 w-full">
-      <div className="h-full w-full">
-        <h2>Seção 2</h2>
-        <p>
-          Bem-vindo à Seção agr. Este é um exemplo simples de um componente de
-          seção no Next.js. Você pode personalizar este componente conforme
-          necessário para atender aos requisitos da sua aplicação.
-        </p>
+    <section
+      id="sobre"
+      className="flex flex-col bg-background z-50 relative rounded-t-3xl"
+    >
+      <div className="sticky top-20 flex flex-row px-6">
+        <BadgeContent content="sobre" />
       </div>
+      <LazyMotion features={domAnimation}>
+        <div className="w-screen h-screen p-6 lg:p-12">
+          <m.p className="text-black w-full ">Sobre 1</m.p>
+        </div>
+        <div className="w-screen h-screen p-6 lg:p-12">
+          <m.p className="text-black w-full ">Sobre 2</m.p>
+        </div>
+      </LazyMotion>
     </section>
   );
 };
