e7b615c59b563e90b01cce63a6bee42386069480
cbxcvl <carloscavallinburatto@gmail.com>
2024-02-07 16:52:17 -0300

    feat: add  home button component

--- src/components/HomeButton/HomeButton.tsx
+++ src/components/HomeButton/HomeButton.tsx
@@ -7,26 +7,25 @@ interface HomeButtonProps {
 
 export function HomeButton({ content }: HomeButtonProps): JSX.Element {
   return (
-    <Link
-      href="#"
-      className="size-24 lg:size-28 flex fixed items-center justify-start lg:justify-center lg:ml-40 no-underline"
-    >
+    <div className=" flex fixed items-center justify-start lg:justify-center lg:ml-40  z-[999]">
       <LazyMotion features={domAnimation}>
-        <m.div
-          initial={{ y: -200 }}
-          animate={{
-            y: 0,
-            transition: { stiffness: 500, damping: 60, mass: 1 },
-          }}
-          whileHover={{ scale: 1.3, x: 20 }}
-          whileTap={{ scale: 0.9, x: 20 }}
-          className="items-center justify-center"
-        >
-          <p className="font-sans font-light text-base uppercase text-black">
-            {content}
-          </p>
+        <m.div className="size-24 lg:size-28 items-center justify-start flex">
+          <Link href={'#'} className="no-underline">
+            <m.p
+              initial={{ y: -200 }}
+              animate={{
+                y: 0,
+                transition: { stiffness: 500, damping: 60, mass: 1 },
+              }}
+              whileHover={{ scale: 1.3, x: 20 }}
+              whileTap={{ scale: 1.3, x: 20 }}
+              className="font-sans font-semibold text-base uppercase text-black"
+            >
+              {content}
+            </m.p>
+          </Link>
         </m.div>
       </LazyMotion>
-    </Link>
+    </div>
   );
 }
