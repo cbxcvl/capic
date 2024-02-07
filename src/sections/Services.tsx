0f2cde56c45c81dabfd5a77216f7c9fcf7965882
cbxcvl <carloscavallinburatto@gmail.com>
2024-02-07 16:53:37 -0300

    feat: add services to page

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -1,12 +1,26 @@
+'use client';
+
+import { LazyMotion, domAnimation, m } from 'framer-motion';
+
+import { BadgeContent } from '../components/BadgeContent/BadgeContent';
+
 export const Services = () => {
   return (
-    <section id="servicos">
-      <h2>Seção 3</h2>
-      <p>
-        Bem-vindo à Seção 3. Este é um exemplo simples de um componente de seção
-        no Next.js. Você pode personalizar este componente conforme necessário
-        para atender aos requisitos da sua aplicação.
-      </p>
+    <section
+      id="servicos"
+      className="flex flex-col bg-background z-50 relative rounded-t-3xl"
+    >
+      <div className="sticky top-20 flex flex-row-reverse px-6">
+        <BadgeContent content="serviços" />
+      </div>
+      <LazyMotion features={domAnimation}>
+        <div className="w-screen h-screen p-6 lg:p-12">
+          <m.p className="text-black w-full ">Serviço 1</m.p>
+        </div>
+        <div className="w-screen h-screen p-6 lg:p-12">
+          <m.p className="text-black w-full ">Serviço 2</m.p>
+        </div>
+      </LazyMotion>
     </section>
   );
 };
