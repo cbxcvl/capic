4675896fda53ff24caa1fe1ef58ca431a66333f8
cbxcvl <carloscavallinburatto@gmail.com>
2024-02-07 16:51:58 -0300

    feat: add  badge content component

---
+++ src/components/BadgeContent/BadgeContent.tsx
@@ -0,0 +1,15 @@
+interface ContentProps {
+  content: string;
+}
+
+export function BadgeContent({ content }: ContentProps): JSX.Element {
+  return (
+    <div
+      className={`sticky top-20 size-24 lg:size-28 items-center justify-start flex `}
+    >
+      <p className="font-sans font-semibold text-base uppercase text-black">
+        ({content})
+      </p>
+    </div>
+  );
+}
