faa3751fc4cd40e6bf8a26a2150adc82fde3fbdb
cbxcvl <carloscavallinburatto@gmail.com>
2024-02-23 17:57:26 -0300

    Fix content rendering in BadgeContent component

--- src/components/BadgeContent/BadgeContent.tsx
+++ src/components/BadgeContent/BadgeContent.tsx
@@ -6,7 +6,7 @@ export function BadgeContent({ content }: ContentProps): JSX.Element {
   return (
     <div className="top-20 size-24 lg:size-28 items-center justify-start flex pl-6 lg:pl-12 ">
       <p className="font-sans font-semibold text-base uppercase text-black">
-        ({content})
+        {content}
       </p>
     </div>
   );
