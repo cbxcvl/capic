432c09224ad3ad159c7f329d148447eb141c160f
cbxcvl <carloscavallinburatto@gmail.com>
2024-02-23 16:36:08 -0300

    Update SectionContent styling and font size

--- src/components/SectionContent/SectionContent.tsx
+++ src/components/SectionContent/SectionContent.tsx
@@ -6,8 +6,8 @@ interface ScrollContentProps {
 
 export function SectionContent({ content }: ScrollContentProps) {
   return (
-    <div className="w-[80%] h-[40%] pt-24 ">
-      <p className="text-black w-fit h-full text-2xl font-medium font-sans text-start scale-[scrollYProgress]">
+    <div className="w-[80%] h-[40%] lg:pl-24 pt-24 lg:w-[60%] lg:h-[30%] ">
+      <p className="text-black text-2xl font-medium font-sans text-start md:text-4xl ">
         {content}
       </p>
     </div>
