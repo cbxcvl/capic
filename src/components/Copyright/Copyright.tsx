807069391dacbe735f6f5d3587288b182723b255
cbxcvl <carloscavallinburatto@gmail.com>
2024-02-23 17:57:39 -0300

    Update link in Copyright component

--- src/components/Copyright/Copyright.tsx
+++ src/components/Copyright/Copyright.tsx
@@ -1,14 +1,11 @@
 export function Copyright(): JSX.Element {
-  const handleClick = () => {
-    window.open('https://github.com/cbxcvl', '_blank');
-  };
-
   return (
     <div className="items-center justify-end pr-6 flex flex-row gap-3">
       <p className="font-sans text-background">© 2024 by</p>
       <a
-        className="font-sans uppercase text-redContent hover:underline visited:text-redContent"
-        onClick={handleClick}
+        className="font-sans uppercase no-underline text-redContent hover:underline"
+        href="https://www.github.com/cbxcvl/"
+        target="_blank"
       >
         cbxcvl
       </a>
