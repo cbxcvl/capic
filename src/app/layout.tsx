021a13aad72c1ccfeb90d525c8e7adf39edac40a
cbxcvl <carloscavallinburatto@gmail.com>
2024-02-26 01:32:30 -0300

    Remove unused imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
   title: 'Capic | Capi Corporation',
