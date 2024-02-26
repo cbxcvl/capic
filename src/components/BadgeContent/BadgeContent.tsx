288b2fc734c43d4c4245119705897462d8b00e2a
cbxcvl <carloscavallinburatto@gmail.com>
2024-02-26 19:43:10 -0300

    Update font size for BadgeContent component

--- src/components/BadgeContent/BadgeContent.tsx
+++ src/components/BadgeContent/BadgeContent.tsx
@@ -5,7 +5,7 @@ interface ContentProps {
 export function BadgeContent({ content }: ContentProps): JSX.Element {
   return (
     <div className="top-20 size-24 lg:size-28 items-center justify-start flex pl-6 lg:pl-12 ">
-      <p className="font-sans font-semibold text-base uppercase text-black">
+      <p className="font-sans font-semibold text-base uppercase text-black lg:text-xl">
         {content}
       </p>
     </div>
