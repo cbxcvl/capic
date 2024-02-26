871969282d53a19a1a36c8e89eeb549e1bfb3d0b
cbxcvl <carloscavallinburatto@gmail.com>
2024-02-26 19:43:26 -0300

    Update Services section layout and content

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -33,11 +33,14 @@ export const Services = () => {
       id="servicos"
       className="flex flex-col bg-background z-50 relative"
     >
-      <div className=" sticky top-20 flex flex-row-reverse pr-12">
-        <BadgeContent content="serviços" />
+      <div className=" sticky top-20 lg:top-36 flex flex-row-reverse pr-12 lg:pr-40">
+        <BadgeContent content="(serviços)" />
       </div>
       <LazyMotion features={domAnimation}>
-        <div ref={ref1} className="w-full h-screen p-6 lg:p-12 flex">
+        <div
+          ref={ref1}
+          className="w-full lg:w-[70%] h-screen pl-6 pt-6 lg:pt-12 flex lg:pl-0 lg:self-center "
+        >
           <m.div
             animate={controls1}
             initial={{ opacity: 0.3 * 1.618, scale: 0.9 }}
@@ -50,7 +53,10 @@ export const Services = () => {
             <SectionContent content="É a representação digital da credibilidade de uma empresa. Fornecendo de maneira concisa história, missão, visão, produtos e serviços, ele se torna central para clientes e parceiros em busca de informações confiáveis. A importância desse espaço virtual? É a solidez que assegura confiança e profissionalismo online" />
           </m.div>
         </div>
-        <div ref={ref2} className="w-full h-screen p-6 lg:p-12 flex">
+        <div
+          ref={ref2}
+          className="w-full lg:w-[70%] h-screen pl-6 pt-6 lg:pt-12 flex lg:pl-0 lg:self-center"
+        >
           <m.div
             animate={controls2}
             initial={{ opacity: 0.3, scale: 0.9 }}
