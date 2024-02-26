ad49cca455857c40fb93e3d4e17f13967f8334ba
cbxcvl <carloscavallinburatto@gmail.com>
2024-02-26 02:12:56 -0300

    Refactor dynamic imports in Services component

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -5,19 +5,9 @@ import { useInView } from 'react-intersection-observer';
 import { LazyMotion, domAnimation, m } from 'framer-motion';
 import dynamic from 'next/dynamic';
 
-const BadgeContent = dynamic(() =>
-  import('../components/BadgeContent/BadgeContent').then((m) => m.BadgeContent),
-);
-
-const SectionContent = dynamic(() =>
-  import('../components/SectionContent/SectionContent').then(
-    (m) => m.SectionContent,
-  ),
-);
-
-const Square = dynamic(() =>
-  import('../components/Square/Square').then((m) => m.Square),
-);
+import { BadgeContent } from '../components/BadgeContent/BadgeContent';
+import { SectionContent } from '../components/SectionContent/SectionContent';
+import { Square } from '../components/Square/Square';
 
 export const Services = () => {
   const controls1 = useAnimation();
