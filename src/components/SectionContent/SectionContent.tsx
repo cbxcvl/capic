5c2cfa32c95d778563f81b862baa8bb86269c3fa
cbxcvl <carloscavallinburatto@gmail.com>
2024-03-06 18:43:11 -0300

    Update SectionContent styling

--- src/components/SectionContent/SectionContent.tsx
+++ src/components/SectionContent/SectionContent.tsx
@@ -4,8 +4,8 @@ interface ScrollContentProps {
 
 export function SectionContent({ content }: ScrollContentProps) {
   return (
-    <div className="w-[90%] h-[40%] lg:pl-24 pt-24 lg:w-[100%] ">
-      <p className="text-black text-2xl font-medium font-sans text-start md:text-4xl ">
+    <div className="w-[80%] h-[40%] lg:pl-24 pt-24 lg:w-[100%] ">
+      <p className="text-black text-2xl font-medium font-sans text-left md:text-4xl ">
         {content}
       </p>
     </div>
