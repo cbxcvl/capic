a683628970b034807a2991fda12546ca6cab7888
cbxcvl <carloscavallinburatto@gmail.com>
2024-03-12 17:44:07 -0300

    Remove unused URLs from sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,20 +7,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       priority: 1,
     },
-    {
-      url: 'https://capic.com/#about',
-      lastModified: new Date(),
-      priority: 0.8,
-    },
-    {
-      url: 'https://capic.com/#services',
-      lastModified: new Date(),
-      priority: 0.8,
-    },
-    {
-      url: 'https://capic.com/#contact',
-      lastModified: new Date(),
-      priority: 0.8,
-    },
   ];
 }
